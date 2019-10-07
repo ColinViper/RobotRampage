979b5e8de812d9db15439b11250675ba4948dd9e
Colin Labelle <colinlabelle777@gmail.com>
2019-10-07 15:35:18 -0400

    Robot Rampage chpt 10, Animating robots

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -18,6 +18,7 @@ public class Robot : MonoBehaviour
     private Transform player;
     private float timeLastFired;
     private bool isDead;
+    public Animator robot;
 
     // Start is called before the first frame update
     void Start()
@@ -51,6 +52,6 @@ public class Robot : MonoBehaviour
 
     private void fire()
     {
-        Debug.Log("Fire");
+        robot.Play("Fire");
     }
 }
